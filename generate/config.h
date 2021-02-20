956c03bff87e756fab1365b9093ee94adbd946a9
DreamPiggy <lizhuoli1126@126.com>
2021-02-20 15:19:20 +0800

    Fix the watchOS simulator build issue. Disable the caching at all.

--- generate/config.h
+++ generate/config.h
@@ -1,14 +1,21 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+#import <TargetConditionals.h>
+
 // disable JSON assert
 #define RAPIDJSON_ASSERT
 
 // enable threading
-#if __i386__
-// Apple Platform i386 does not have Thread Local support
+#if TARGET_OS_SIMULATOR
+// Apple Platform iOS 9/watchOS 2 simulator does not have Thread Local support
+  #if TARGET_OS_IOS && (__IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_10_0)
+  #elif TARGET_OS_WATCH && (__WATCH_OS_VERSION_MIN_REQUIRED < __WATCHOS_3_0)
+  #else
+    #define LOTTIE_THREAD_SUPPORT
+  #endif
 #else
-#define LOTTIE_THREAD_SUPPORT
+  #define LOTTIE_THREAD_SUPPORT
 #endif
 
 // enable logging
@@ -18,6 +25,6 @@
 //#define LOTTIE_IMAGE_MODULE_SUPPORT
 
 // enable lottie model caching
-#define LOTTIE_CACHE_SUPPORT
+//#define LOTTIE_CACHE_SUPPORT
 
 #endif  // CONFIG_H
